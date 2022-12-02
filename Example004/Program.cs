dce823aba05e8b90520a07daede0271511a9f5c7
sofia <sonrud@mail.ru>
2022-12-02 20:19:39 +0300

    task4

---
+++ Example004/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int i= 1;
+while (i<=n)
+{
+   if (i%2==0)
+    Console.Write($"Результат {i}\n");
+    i++;
+}
