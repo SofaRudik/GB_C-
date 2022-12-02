28c0e4cfeb8cc3dbf7b5c5bf85f55b8832df5f3b
sofia <sonrud@mail.ru>
2022-12-02 19:32:59 +0300

    task2

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -3,7 +3,7 @@ Console.Write("Введите 1-ое число: ");
 int n = int.Parse(Console.ReadLine());
 Console.Write("Введите 2-ое число: ");
 int m = int.Parse(Console.ReadLine());
-Console.Write("Введите 2-ое число: ");
+Console.Write("Введите 3-ое число: ");
 int k = int.Parse(Console.ReadLine());
 int max = n;
 if( max<m )
