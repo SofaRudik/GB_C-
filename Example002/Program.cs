4eb035e4cd1371b569bf59308d353c57110df65c
sofia <sonrud@mail.ru>
2022-12-02 19:31:08 +0300

    task2

---
+++ Example002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int k = int.Parse(Console.ReadLine());
+int max = n;
+if( max<m )
+   max=m;
+if (max<k)
+ max=k;
+Console.Write($"Max= {max}");
